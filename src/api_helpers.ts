2b5926a05a1664fca2c8422f0626a5ab05195eaa
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-03-18 23:28:05 +0200

    done

--- src/api_helpers.ts
+++ src/api_helpers.ts
@@ -1,6 +1,4 @@
-const hiddenApiKey = window.btoa('62cb1aff129ffa5f0374b5be5fad9256');
-
-export const API_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${window.atob(hiddenApiKey)}&id=`;
+export const API_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${window.atob('NjJjYjFhZmYxMjlmZmE1ZjAzNzRiNWJlNWZhZDkyNTY=')}&id=`;
 
 export const getData = async (url: string) => {
   const response = await fetch(url);
