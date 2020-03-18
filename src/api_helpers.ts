60959bcad083d7b9d1363f703b1915e3f4f72014
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-03-18 22:29:13 +0200

    done

--- src/api_helpers.ts
+++ src/api_helpers.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=62cb1aff129ffa5f0374b5be5fad9256&id=';
+export const API_URL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=62cb1aff129ffa5f0374b5be5fad9256&id=';
 
 export const getData = async (url: string) => {
   const response = await fetch(url);
